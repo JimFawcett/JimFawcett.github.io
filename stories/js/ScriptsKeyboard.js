c6e6a15cc3fee6deaaf6611b5d38edbed96a8f80
James W. Fawcett <jfawcett@twcny.rr.com>
2019-08-11 14:17:24 -0400

    lots of changes to C++ story content and sequencer

---
+++ stories/js/ScriptsKeyboard.js
@@ -0,0 +1,43 @@
+/////////////////////////////////////////////////////////////////////
+// ScriptsKeyboard.js - defines behaviors for selected key presses //
+//                                                                 //
+// Jim Fawcett, 25 June 2018                                       //
+/////////////////////////////////////////////////////////////////////
+
+var HelpWin
+
+function keyAction(keyEvent) {
+  keystate = 'down';
+  var key = String.fromCharCode(keyEvent.which);
+  if (key === 'R') {
+    location.reload();
+  }
+  if (key === 'L') {
+    document.getElementById("Lect").click();
+  }
+  if (key === 'C') {
+    document.getElementById("Cour").click();
+  }
+  if (key === 'T')
+    window.scrollTo(0, 0);
+  if (key === 'B')
+    window.scrollTo(0, 10000);
+  if (key === 'U')
+    window.scrollBy(0, -200);
+  if (key === 'D')
+    window.scrollBy(0, 200);
+  if (key === 'N') {
+    document.getElementById("Next").click();
+  }
+  if (key === 'P') {
+    document.getElementById('Prev').click();
+  }
+  if (key === 'H') {
+    HelpWin = window.open('Help.html', 'target=_blank',  'location=yes, width=900, height=650, resizable');
+  }
+};
+
+
+function helpWin() {
+  window.open('Help.html', 'target=_blank', 'location=yes, width=900, height=650, resizable');
+}
---
+++ stories/js/ScriptsSlideShow.js
@@ -0,0 +1,22 @@
+
+/* ScriptsSlideShow.js 
+ *
+ * - minor modification of code from w3schools.com
+ */
+
+var slideIndex = 1;
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slideShow");
+  if (n > x.length) { slideIndex = 1; }
+  if (n < 1) { slideIndex = x.length; }
+  for (i = 0; i < x.length; ++i) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
