dcd8604e483cf35144c046e2431c3c94a83b3fd7
James W. Fawcett <jfawcett@twcny.rr.com>
2019-08-18 22:48:09 -0400

    minor style change to menus

--- js/ScriptsMenu.js
+++ js/ScriptsMenu.js
@@ -15,7 +15,7 @@ function initializeMenu() {
   var topMenu = document.getElementsByTagName("nav");
   topMenu[0].innerHTML = "<div class='navbar'>\
     <a href='index.html' class='menuItem'>Home</a>\
-    <a href='Repositories.html' class='menuItem notice'>Repos</a>\
+    <a href='Repositories.html' class='menuItem darkItem' style='margin:0px;'>Repos</a>\
     <div class='dropdown menuItem'>\
       <button class='dropbutton'>Notes &#9662;</button>\
       <div class='dropdown-content'>\
