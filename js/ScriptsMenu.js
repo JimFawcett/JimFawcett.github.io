3f656ba9719a2343ad4416adef6938a18e94d232
James W. Fawcett <jfawcett@twcny.rr.com>
2019-09-16 16:32:14 -0400

    fixed linke error

--- js/ScriptsMenu.js
+++ js/ScriptsMenu.js
@@ -106,7 +106,7 @@ function initializeMenu() {
         <div class='darkTheme'>&nbsp;Ordered&nbsp;Content</div>\
         <a href='Stories.html'>What is a Story?</a>\
         <a href='StoryTeller.html'>Load Story List with ST</a>\
-        <a href='StoryTeller.html'>Load Story List with ST2</a>\
+        <a href='StoryTeller2.html'>Load Story List with ST2</a>\
         <a class='disable' href='#'>C++ Ecosystem</a>\
         <a class='disable' href='#'>Site Story</a>\
         <a class='disable' href='#'>Repository&nbsp;Content</a>\
