dec275f7308c7e63ac5ff45271124cfeb0024c2f
James W. Fawcett <jfawcett@twcny.rr.com>
2020-01-17 09:32:48 -0500

    changed chaps menu

--- js/ScriptsStory.js
+++ js/ScriptsStory.js
@@ -1,6 +1,7 @@
 // ScriptsStory.js
 
 var storyMenu = new Object();
+
 storyMenu.chaps = function () {
   var menu = document.getElementById("chaps");
   if (isDefined(menu)) {
@@ -9,5 +10,19 @@ storyMenu.chaps = function () {
     else
       menu.style.display = "none";
   }
-}
+};
 
+storyMenu.sects = function () {
+  var menu = document.getElementById("sects");
+  if (isDefined(menu)) {
+    if (menu.style.display !== "block")
+      menu.style.display = "block";
+    else
+      menu.style.display = "none";
+  }
+};
+
+storyMenu.closeTOC = function () {
+  let toc = document.getElementById("toc");
+  toc.style.display = "none";
+};
