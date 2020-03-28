0e0d0625087855b56dd0c736d977512a3805ce36
James W. Fawcett <jfawcett@twcny.rr.com>
2020-03-28 13:25:49 -0400

    added exercises for several Rust Story chapters

--- js/ScriptsMenu.js
+++ js/ScriptsMenu.js
@@ -93,6 +93,7 @@ function initializeMenu() {
         <div class='darkItem maxWidth'>&nbsp;Site&nbsp;Neighborhoods&nbsp;-&nbsp;related&nbsp;content&nbsp;&nbsp;</div>\
         <a href='https://JimFawcett.github.io'>JimFawcett-github</a>\
         <a href='https://www.ecs.syr.edu/faculty/fawcett'>JimFawcett-SyracuseUniv</a>\
+        <a href='https://mwcorley79.github.io/MikeCorley/'>Mike Corley</a>\
         <a href='https://csiac.org'>CSIAC</a>\
         <a href='https://www.fluentcpp.com'>Fluent {C++}</a>\
         <a href='https://www.boost.org/'>Boost&nbsp;Library&nbsp;(C++)</a>\
