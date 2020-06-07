4c9898809dcb09f0aae9a976c658079a5e667a98
James W. Fawcett <jfawcett@twcny.rr.com>
2020-06-07 08:51:36 -0400

    revised small pieces of text and code

--- Resources/RustBites/CodeSamples/ownership/src/main.rs
+++ Resources/RustBites/CodeSamples/ownership/src/main.rs
@@ -1,3 +1,8 @@
+/////////////////////////////////////////////////////////////
+// ownership::main.rs - demonstrates Rust ownership        //
+//                                                         //
+// Jim Fawcett, https://JimFawcett.github.com, 07 Jun 2020 //
+/////////////////////////////////////////////////////////////
 
 /*-- only one writer in function scope --*/
 fn mutate_and_show(t:&mut String) {
@@ -7,12 +12,14 @@ fn mutate_and_show(t:&mut String) {
 }
 
 fn main() {
+    // Blk #1
     /*-- multiple readers: O.K. --*/
     let s = String::from("immutable string");
     let t = &s;
     print!("\n  s = {:?}", s);
     print!("\n  t = {:?}", t);
 
+    // Blk #2
     /*-- one writer: O.K. --*/
     let mut u = String::from("mutable string");
     u.push_str(" u");
@@ -22,19 +29,20 @@ fn main() {
        attempting to add reader v with active writer u
        not O.K. 
     */
-    // let v = &u;
-    // u.push_str("modfied by u");
-    // print!("\n  v = {:?}", v);
+   //  let v = &u;
+   //  u.push_str("modfied by u");
+   //  print!("\n  v = {:?}", v);
    
+    // Blk #3
     /*-- mutable v borrows u's ability to write, O.K. --*/
     let v = &mut u;
     v.push_str(" referenced by v");
     print!("\n  v = {:?}", v);
 
     /*-- attempting to use second writer, not O.K. --*/
-    // u.push_str(" and modifed again by u");
-    // print!("\n  u = {:?}", u);
-    // print!("\n  v = {:?}", v);
+   //  u.push_str(" and modifed again by u");
+   //  print!("\n  u = {:?}", u);
+   //  print!("\n  v = {:?}", v);
 
     /*-----------------------------------------------------
        can mutate u in function since ref v is not accessible 
@@ -45,7 +53,7 @@ fn main() {
        attempting to use v after borrowing u in function call.
        Not O.K.
        */
-    v.push_str(" modified by v");
+   //  v.push_str(" modified by v");
 
     println!("\n\n  That's all Folks!\n\n");
 }
