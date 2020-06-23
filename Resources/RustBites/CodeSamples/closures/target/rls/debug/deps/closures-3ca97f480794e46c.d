1f76ce5a115a5599fb971baa027d0927dc32ed0f
James W. Fawcett <jfawcett@twcny.rr.com>
2020-06-23 10:25:08 -0400

    added methods

--- Resources/RustBites/CodeSamples/closures/target/rls/debug/deps/closures-3ca97f480794e46c.d
+++
@@ -1,5 +0,0 @@
-c:\github\JimFawcett\JimFawcett.github.io\Resources\RustBites\CodeSamples\closures\target\rls\debug\deps\closures-3ca97f480794e46c.rmeta: src\main.rs
-
-c:\github\JimFawcett\JimFawcett.github.io\Resources\RustBites\CodeSamples\closures\target\rls\debug\deps\closures-3ca97f480794e46c.d: src\main.rs
-
-src\main.rs:
--- Resources/RustBites/CodeSamples/closures/target/rls/debug/deps/closures-b1dfe954b28f72a5.d
+++
@@ -1,5 +0,0 @@
-c:\github\JimFawcett\JimFawcett.github.io\Resources\RustBites\CodeSamples\closures\target\rls\debug\deps\closures-b1dfe954b28f72a5.rmeta: src\main.rs
-
-c:\github\JimFawcett\JimFawcett.github.io\Resources\RustBites\CodeSamples\closures\target\rls\debug\deps\closures-b1dfe954b28f72a5.d: src\main.rs
-
-src\main.rs:
