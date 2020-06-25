880a7a95f5c0e927117bb68d02d990e7ad23a3cd
James W. Fawcett <jfawcett@twcny.rr.com>
2020-06-24 20:32:31 -0400

    completed point demo

--- Resources/RustBites/CodeSamples/point/examples/test1.rs
+++ Resources/RustBites/CodeSamples/point/examples/test1.rs
@@ -1,19 +1,54 @@
-// point::test1::main.rs
+/////////////////////////////////////////////////////////////
+// point::test1.rs - Demo Point<T> with                    //
+//                   Time and Display Traits               //
+//                                                         //
+// Jim Fawcett, https://JimFawcett.github.io, 24 Jun 2020  //
+/////////////////////////////////////////////////////////////
 
-extern crate point;
+#![allow(dead_code)]
 use point::{*};
+use point::Time;
+use chrono::{Local, Datelike, Timelike};
 
+fn test_time() {
+    print!("\n  Test Time Evaluation");
+    print!("\n ======================");
+
+    let now = Local::now();
+    let (_is_pm, mut hour) = now.hour12();
+    if _is_pm {
+        hour += 12;
+    }
+    let (_is_common_era, year) = now.year_ce();
+    print!("\n  time is {}", now);
+    print!(
+        "\n  year: {}, month: {:0>2}, day: {:0>2}", 
+        year, now.month(), now.day()
+    );
+    print!(
+        "\n  hour: {:0>2}, min: {:0>2}, sec: {:0>2}", 
+        hour, now.minute(), now.second()
+    );
+    println!();
+}
 fn main() {
+
+    // test_time();
 
     print!("\n  Demonstrating Point Type");
     print!("\n ==========================");
 
+    print!("\n  -- create Point<i32> and display with Debug format --");
     let mut pt = Point::<i32>::new();
     pt.set_coordinates(&[1, 2, 3]);
+    pt.set_time(Local::now());
     print!("\n  pt = {:?}", &pt);
+    print!("\n  -- display coordinates and time with Display format --");
     print!("\n  coordinates are: {:?}", &pt.get_coordinates());
+    print!("\n  point time is: {}", &pt.get_time_string());
+    print!("\n  -- mutate coordinates and display again --");
     pt.set_coordinates(&[3, 2, 1]);
-    print!("\n  pt = {:?}", pt);
+    print!("\n  pt = {}", pt);
     print!("\n  coordinates are: {:?}", pt.get_coordinates());
 
     println!("\n\n  That's all Folks!\n\n");
--- Resources/RustBites/CodeSamples/point/src/lib.rs
+++ Resources/RustBites/CodeSamples/point/src/lib.rs
@@ -1,62 +1,109 @@
+/////////////////////////////////////////////////////////////
+// point::lib.rs - Demo Traits                             //
+//                                                         //
+// Jim Fawcett, https://JimFawcett.github.io, 24 Jun 2020  //
+/////////////////////////////////////////////////////////////
 
-extern crate chrono;
 use chrono::offset::Local;
 use chrono::DateTime;
+use chrono::{Datelike, Timelike};
 use std::fmt::*;
-use std::time::SystemTime;
 
-trait Time {
-    //fn set_time_now(&mut self);
-    fn set_time(&mut self, st: SystemTime);
-    fn get_time(&self) -> SystemTime;
+
+pub trait Time {
+    fn set_time(&mut self, st: DateTime<Local>);
+    fn get_time(&self) -> DateTime<Local>;
     fn get_time_string(&self) -> String;
 }
 
 #[derive(Debug)]
 pub struct Point<T> 
 where T:Default + Debug {
-    x:T, y:T, z:T, t:SystemTime,
+    x:T, y:T, z:T, t:DateTime<Local>,
 }
+/*-- implement Time trait --*/
 impl<T> Time for Point<T>
 where T:Default + Debug + Clone {
-  fn set_time(&mut self, st: SystemTime) {
-    self.t = st;
+    fn set_time(&mut self, st: DateTime<Local>) {
+        self.t = st;
   }
-  fn get_time(&self) -> SystemTime {
+  fn get_time(&self) -> DateTime<Local> {
       self.t
   }
   fn get_time_string(&self) -> String {
-      let datetime: DateTime<Local> = self.t.into();
-      datetime.format("%d/%m/%Y %T");
-      datetime.to_string()
+      let year = self.t.year().to_string();
+      let mon = self.t.month().to_string();
+      let day = self.t.day().to_string();
+      let hour = self.t.hour().to_string();
+      let min = self.t.minute().to_string();
+      let sec = self.t.second().to_string();
+      let dt = format!(
+          "{}::{:0>2}::{:0>2} {:0>2}::{:0>2}::{:0>2}", 
+          year, mon, day, hour, min, sec
+      );
+      dt
   }
 }
+/*-- implement Display trait --*/
+impl<T> Display for Point<T>
+where T:Default + Debug + Clone {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        write!(
+            f, "{{ {:?}, {:?}, {:?}, {} }}", 
+            &self.x, &self.y, &self.z, 
+            &self.get_time_string()
+        )
+    }
+}
+/*-- implement Point methods --*/
 impl<T> Point<T> where T:Default + Debug + Clone {
     pub fn new() -> Point<T> {
         Point {
             x:std::default::Default::default(), 
             y:std::default::Default::default(), 
             z:std::default::Default::default(),
-            t:SystemTime::now()
+            t:Local::now()
         }
     }
     pub fn default() -> Point<T> {
         Point::new()
     }
+    /*-- return array of three spatial coordinates --*/
     pub fn get_coordinates(&self) -> [T; 3] {
         [self.x.clone(), self.y.clone(), self.z.clone()]
     }
+    /*-- set coordinates from array slice --*/
     pub fn set_coordinates(&mut self, coor: &[T; 3]) {
         self.x = coor[0].clone();
         self.y = coor[1].clone();
         self.z = coor[2].clone();
-    } 
+    }
+    /*-- time is returned with Time::get_time() --*/ 
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn construct () {
+        let pt = Point::<i32>::new();
+        assert_eq!(pt.get_coordinates(), [0, 0, 0]);
+    }
+    #[test]
+    fn set_coor() {
+        let mut pt = Point::<i32>::new();
+        pt.set_coordinates(&[1, 2, 3]);
+        assert_eq!(pt.get_coordinates(), [1, 2, 3]);
+    }
+    #[test]
+    fn get_time() {
+        use chrono::{Duration};
+        let two_sec = Duration::seconds(2);
+        let ts_start = Local::now() - two_sec;
+        let pt = Point::<i32>::new();
+        let ts_now = pt.get_time();
+        let ts_stop = Local::now() + two_sec;
+        assert!(ts_start < ts_now);
+        assert!(ts_now < ts_stop);
     }
 }
