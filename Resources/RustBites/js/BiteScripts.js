e7d0f65aa1ac5e4ee8ab25b15bd420c321668768
James W. Fawcett <jfawcett@twcny.rr.com>
2021-02-07 20:47:01 -0500

    added Options Bite

--- Resources/RustBites/js/BiteScripts.js
+++ Resources/RustBites/js/BiteScripts.js
@@ -66,6 +66,7 @@ storyMenu.bitesMenu = function () {
     <a href="RustBites_LifeTime.html">LifeTime</a><br />\
     <a href="RustBites_Abstract.html">Abstractions</a><br />\
     <a href="RustBites_ErrHnd.html">ErrHandling</a><br />\
+    <a href="RustBites_Options.html">Options</a><br />\
     <a class="undefined" href="RustBites_Collects.html">Collections</a><br />\
     <a class="undefined" href="RustBites_Iterator.html">Iterators</a><br />\
     <a class="undefined" href="RustBites_Threads.html">Threads</a><br />\
