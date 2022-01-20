86642f8a1c06052791a1c1e4573ef76cb2f3a56c
James W. Fawcett <jfawcett@twcny.rr.com>
2022-01-20 14:24:32 -0500

    minor changes to the sections and pages display content

--- js/ScriptsFlexMenusPages.js
+++ js/ScriptsFlexMenusPages.js
@@ -4,36 +4,39 @@ function initializePages() {
 
   var pagesMenu = document.getElementById("pages");
   if (isDefined(pagesMenu)) {
-    pagesMenu.innerHTML = "<div class='pageItems'>\
-          <div style='height:0.5em;'>&nbsp;</div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
-          <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
+    pagesMenu.innerHTML = "<div style='height:0.5em;'>&nbsp;</div>\
           <div class='ddItem'><a href='TestFlexMenu.html'>TestFlexMenu</a></div>\
           <div class='ddItem'><a href='TestSiblingFlexMenu.html'>TestSiblingFlexMenu&nbsp;&nbsp;</a></div>\
-          <div style='height:0.25em'>&nbsp;</div>\
-    </div > ";
+          <div class='undef ddItem'><a href='page3.html'>Page #3</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #4</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #5</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #6</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #7</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #8</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #9</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #10</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #11</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #12</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #13</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #14</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #15</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #16</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #17</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #18</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #19</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #20</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #21</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #22</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #23</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #24</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #25</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #26</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #27</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #28</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #29</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #30</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #31</a></div>\
+          <div class='undef ddItem'><a href='page3.html'>Page #32</a></div>\
+          <div style='height:0.25em'>&nbsp;</div>";
   }
 }
