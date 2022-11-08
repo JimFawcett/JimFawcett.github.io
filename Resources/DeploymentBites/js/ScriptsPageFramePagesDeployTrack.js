7b85e69bdf83f3ba9f4ef73660508277cf9fef5e
Jim Fawcett <fawcettjim@outlook.com>
2022-11-08 10:28:22 -0600

    started Deploy_git

---
+++ Resources/DeploymentBites/js/ScriptsPageFramePagesDeployTrack.js
@@ -0,0 +1,25 @@
+/*
+ * ScriptsPageFramePages.js
+ */
+
+function initializePages() {
+
+  var pagesMenu = document.getElementById("pages");
+  var pgbtn = document.getElementById("pgbtn");
+  if (isDefined(pagesMenu)) {
+    pgbtn.style.display = "flex";
+    pagesMenu.innerHTML = "<menu-elem class='ddItems pageElem undef'><a href='Javascript:;'>Deploy&nbsp;Bites&nbsp;Repo</a>&nbsp;</menu-elem>\
+          <menu-elem class='ddItems pageElem'><a href='../../indexDeployment.html'>Deployment&nbsp;Track&nbsp;Summary</a>&nbsp;</menu-elem>\
+          <menu-elem class='ddItems pageElem'><a href='DeployBites_Git.html'>git</a></menu-elem>\
+          <menu-elem class='ddItems pageElem undef'><a href='# DeployBites_Github.html'>github</a></menu-elem>\
+          <menu-elem class='ddItems pageElem undef'><a href='# DeployBites_Containers.html'>Containers</a></menu-elem>\
+          <menu-elem class='ddItems pageElem undef'><a href='# DeployBites_Cloud.html'>Cloud</a></menu-elem>\
+          <menu-elem class='ddItems pageElem undef'><a href='# DeployBites_PWA.html'>PWA</a></menu-elem>\
+          <menu-elem>&nbsp;</div>\
+          <menu-elem>&nbsp;</div>";
+  }
+  else {
+    var pgbtn = document.getElementById("pgbtn");
+    pgbtn.style.display = "none";
+  }
+}
