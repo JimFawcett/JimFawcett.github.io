8efec4e46893860e8a5e2c21c9eef8f59f0577bd
Jim Fawcett <fawcettjim@outlook.com>
2022-11-29 09:39:19 -0600

    added CommComp link to Resrcs dropdown

--- js/ScriptsPageFrame.js
+++ js/ScriptsPageFrame.js
@@ -234,6 +234,7 @@ function initializeMenu() {
               <div style='height:0.25em;'></div>\
             </div>\
           </details>\
+          <div class='ddItem'><a href='CommCompare.html' class='menuItem'>CommCompare:&nbsp;Rust&nbsp;&amp;&nbsp;C++</a></div>\
           <details style='margin-left:0.5em; cursor:pointer;' class='ddItem'>\
             <summary>Tests</summary>\
             <div style='margin-left:1.0em;'>\
