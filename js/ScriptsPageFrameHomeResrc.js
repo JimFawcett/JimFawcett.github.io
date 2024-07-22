b7319aeaf23e8a429519a927c7597e1ee3e5130c
Jim Fawcett <fawcettjim@outlook.com>
2024-07-21 21:47:22 -0500

    changed a few anchor targets

--- js/ScriptsPageFrameHomeResrc.js
+++ js/ScriptsPageFrameHomeResrc.js
@@ -402,8 +402,10 @@ function initializeMenu() {
     <div class='menuItem'>\
         Pages&#9662\
         <div class='dropdown'>\
-          <div class='ddLabel darkItem'>Site&nbsp;thread</div>\
+          <div class='ddLabel darkItem'>Site&nbsp;Resource&nbsp;pages</div>\
             <div style='height:0.0em;'></div>\
+            <div class='ddItem'><a href='index.html'>Home</a></div>\
+            <div class='ddItem'><a target='_blank' href='https://github.com/JimFawcett'>github Repository</a></div>\
             <div class='ddItem'><a href='SiteGlossary.html'>Glossary</a></div>\
             <div class='ddItem'><a href='SiteReferences.html'>References</a></div>\
             <div class='ddItem'><a href='SiteAdditions.html'>Additions</a></div>\
--- js/ScriptsPageFramePagesHomeResrc.js
+++ js/ScriptsPageFramePagesHomeResrc.js
@@ -8,12 +8,15 @@ function initializePages() {
   /*var pgbtn = document.getElementById("pgbtn");*/
   if (isDefined(pagesMenu)) {
     pagesMenu.style.overflowX = "hidden";
-    pagesMenu.innerHTML = "<div class='darkItem popupHeader' onclick='location.reload()'>Pages</div>\
+    pagesMenu.innerHTML = "<div class='darkItem popupHeader' onclick='location.reload()'>Site Resource Pages</div>\
+        <menu-elem class='ddItems pageElem'><a href='index.html'>Home</a></menu-elem>\
+        <menu-elem class='ddItems pageElem'><a href='https://github.com/JimFawcett'>github&nbsp;Repository</a></menu-elem>\
         <menu-elem class='ddItems pageElem'><a href='SiteGlossary.html'>Glossary</a></menu-elem>\
         <menu-elem class='ddItems pageElem'><a href='SiteReferences.html'>References</a>&nbsp;</menu-elem>\
         <menu-elem class='ddItems pageElem'><a href='SiteAdditions.html'>Additions</a>&nbsp;</menu-elem>\
         <menu-elem class='ddItems pageElem'><a href='help.html'>Help</a>&nbsp;</menu-elem>\
         <menu-elem>&nbsp;</div>";
+    pagesMenu.style.width = "11em";
   }
   else {
     var pgbtn = document.getElementById("pgbtn");
