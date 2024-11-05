abbd7fa454fc456911fedf730fe638edfe2191d5
Jim Fawcett <fawcettjim@outlook.com>
2024-11-05 16:07:55 -0600

    cleaned DesignBits/Point

--- Resources/DesignBites/CodeSamples/point/target/debug/deps/point-551fabc755c66d59.d
+++
@@ -1,5 +0,0 @@
-C:\github\JimFawcett\JimFawcett.github.io\Resources\DesignBites\CodeSamples\point\target\debug\deps\libpoint-551fabc755c66d59.rmeta: src\lib.rs
-
-C:\github\JimFawcett\JimFawcett.github.io\Resources\DesignBites\CodeSamples\point\target\debug\deps\point-551fabc755c66d59.d: src\lib.rs
-
-src\lib.rs:
--- Resources/DesignBites/CodeSamples/point/target/debug/deps/windows_x86_64_msvc-46822475e080fc1a.d
+++
@@ -1,5 +0,0 @@
-C:\github\JimFawcett\JimFawcett.github.io\Resources\DesignBites\CodeSamples\point\target\debug\deps\libwindows_x86_64_msvc-46822475e080fc1a.rmeta: C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\src/lib.rs
-
-C:\github\JimFawcett\JimFawcett.github.io\Resources\DesignBites\CodeSamples\point\target\debug\deps\windows_x86_64_msvc-46822475e080fc1a.d: C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\src/lib.rs
-
-C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows_x86_64_msvc-0.52.6\src/lib.rs:
