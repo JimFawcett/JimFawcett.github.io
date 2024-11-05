9be45e9e40a0e41b565b867cae46728a3f7b1ba1
Jim Fawcett <fawcettjim@outlook.com>
2024-11-05 16:06:04 -0600

    added help files

---
+++ Resources/DesignBites/CodeSamples/point/target/debug/build/num-traits-a83472a8059ebae4/build_script_build-a83472a8059ebae4.d
@@ -0,0 +1,5 @@
+C:\github\JimFawcett\JimFawcett.github.io\Resources\DesignBites\CodeSamples\point\target\debug\build\num-traits-a83472a8059ebae4\build_script_build-a83472a8059ebae4.exe: C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs
+
+C:\github\JimFawcett\JimFawcett.github.io\Resources\DesignBites\CodeSamples\point\target\debug\build\num-traits-a83472a8059ebae4\build_script_build-a83472a8059ebae4.d: C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs
+
+C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\num-traits-0.2.19\build.rs:
---
+++ Resources/DesignBites/CodeSamples/point/target/debug/deps/windows_targets-40f4fca1e467951d.d
@@ -0,0 +1,5 @@
+C:\github\JimFawcett\JimFawcett.github.io\Resources\DesignBites\CodeSamples\point\target\debug\deps\libwindows_targets-40f4fca1e467951d.rmeta: C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.52.6\src/lib.rs
+
+C:\github\JimFawcett\JimFawcett.github.io\Resources\DesignBites\CodeSamples\point\target\debug\deps\windows_targets-40f4fca1e467951d.d: C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.52.6\src/lib.rs
+
+C:\Users\fawce\.cargo\registry\src\index.crates.io-6f17d22bba15001f\windows-targets-0.52.6\src/lib.rs:
