57b46e91440a652022440be3eec3f933cb500463
Jim Fawcett <fawcettjim@outlook.com>
2024-12-05 21:50:58 -0600

    cleaning up menu links

--- js/ScriptsPageFramePagesWebDevTrack.js
+++ js/ScriptsPageFramePagesWebDevTrack.js
@@ -8,11 +8,12 @@ function initializePages() {
   /*var pgbtn = document.getElementById("pgbtn");*/
   if (isDefined(pagesMenu)) {
     /*pgbtn.style.display = "flex";*/
-    pagesMenu.innerHTML = "<div class='darkItem popupHeader' onclick='bottomMenu.pages()'>Pages</div>\
-          <menu-elem class='ddItems pageElem'><a href='indexWebDev.html'>WebDev&nbsp;Track&nbsp;Summary</a>&nbsp;</menu-elem>\
-          <menu-elem class='ddItems pageElem'><a class='undefined' href='Javascript:;'>Introduction</a></menu-elem>\
-          <menu-elem class='ddItems pageElem'><a class='undefined' href='Javascript:;'>...</a></menu-elem>\
-          <menu-elem>&nbsp;</menu-elem>\
+    pagesMenu.innerHTML = "<div class='darkItem popupHeader' onclick='bottomMenu.pages()'>&nbsp;WebDev&nbsp;Bites&nbsp;Thread&nbsp;</div>\
+          <menu-elem class='ddItems pageElem'><a target='_parent' href='indexWebDev.html'>Track&nbsp;Summary&nbsp;</a></menu-elem>\
+          <menu-elem class='ddItems pageElem'><a target='_parent' href='Resources/WebDev/WebDev_Survey.html'>Survey</a>&nbsp;</menu-elem>\
+          <menu-elem class='ddItems pageElem'><a target='_parent' href='Resources/WebDev/WebDev_ShowDiag.html'>[show diagram]</a>&nbsp;</menu-elem>\
+          <menu-elem class='ddItems pageElem'><a target='_parent' href='Resources/WebDev/WebDev_MsgSndr.html'>Windows Messages</a>&nbsp;</menu-elem>\
+          <menu-elem class='ddItems pageElem undef'><a target='_parent' href='javascript:;'>more later</a>&nbsp;</menu-elem>\
           <menu-elem>&nbsp;</menu-elem>";
   }
   else {
--- js/ScriptsPageFramePagesWebDevTrackSummary.js
+++ js/ScriptsPageFramePagesWebDevTrackSummary.js
@@ -8,13 +8,12 @@ function initializePages() {
   var pgbtn = document.getElementById("pgbtn");
   if (isDefined(pagesMenu)) {
     pgbtn.style.display = "flex";
-    pagesMenu.innerHTML = "<div class='darkItem popupHeader' onclick='bottomMenu.pages()'>&nbsp;WebDev&nbsp;Track&nbsp;</div>\
-          <menu-elem class='ddItems pageElem'><a target='_parent' href='javascript:;'>Track&nbsp;Summary&nbsp;</a></menu-elem>\
-          <menu-elem class='ddItems pageElem undef'><a target='_parent' href='javascript:;'>Track&nbsp;Repo</a>&nbsp;</menu-elem>\
-          <menu-elem class='ddItems pageElem undef'><a target='_parent' href='javascript:;'>Glossary&nbsp;</a></menu-elem>\
-          <menu-elem class='ddItems pageElem undef'><a target='_parent' href='javascript:;'>References&nbsp;</a></menu-elem>\
-          <menu-elem class='ddItems pageElem undef'><a target='_parent' href='javascript:;'>Flash&nbsp;Cards</a></menu-elem>\
-          <menu-elem class='ddItems pageElem undef'><a target='_parent' href='javascript:;'>Additions&nbsp;</a></menu-elem>\
+    pagesMenu.innerHTML = "<div class='darkItem popupHeader' onclick='bottomMenu.pages()'>&nbsp;WebDev&nbsp;Bites&nbsp;Thread&nbsp;</div>\
+          <menu-elem class='ddItems pageElem'><a target='_parent' href='indexWebDev.html'>Track&nbsp;Summary&nbsp;</a></menu-elem>\
+          <menu-elem class='ddItems pageElem'><a target='_parent' href='Resources/WebDev/WebDev_Survey.html'>Survey</a>&nbsp;</menu-elem>\
+          <menu-elem class='ddItems pageElem'><a target='_parent' href='Resources/WebDev/WebDev_ShowDiag.html'>[show diagram]</a>&nbsp;</menu-elem>\
+          <menu-elem class='ddItems pageElem'><a target='_parent' href='Resources/WebDev/WebDev_MsgSndr.html'>Windows Messages</a>&nbsp;</menu-elem>\
+          <menu-elem class='ddItems pageElem undef'><a target='_parent' href='javascript:;'>more later</a>&nbsp;</menu-elem>\
           <menu-elem>&nbsp;</menu-elem>";
   }
   else {
